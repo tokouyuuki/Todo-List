57d2f4907b40e0e50778f1dae6056208651b42c6
tokouyuuki <tttyuki1050@gmail.com>
2021-10-07 03:01:02 +0900

    追加

--- Todopj/routes/web.php
+++ Todopj/routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', [TodoController::class, 'index']);
 Route::post('/todo/create', [TodoController::class, 'create']);
 Route::post('/todo/update', [TodoController::class, 'update']);
 Route::post('/todo/delete', [TodoController::class, 'delete']);
+//頑張りましょう
